563f0bacfb2dec854c4378ac27725310b756652a
+ <+>
2023-03-17 15:44:37 +0300

    Completed assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,14 +2,14 @@ class PostsController < ApplicationController
 
   def show
     post = Post.find(params[:id])
-    
+
     render json: post
   end
 
   def update
     post = Post.find(params[:id])
 
-    post.update(post_params)
+    post.update!(post_params)
 
     render json: post
   end
@@ -19,5 +19,4 @@ class PostsController < ApplicationController
   def post_params
     params.permit(:category, :content, :title)
   end
-
 end
